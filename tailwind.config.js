039638863b57d60f92af4fe9992ad9d78947054b
Amir3599 <amir.a.koochaki@gmail.com>
2024-10-23 01:57:06 +0330

    Update tailwind.config.js
    update tailwind config
    for install flowbite and add variables for the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,jsx,ts,tsx}", flowbite.content()],
+    theme: {
+        extend: {
+            colors: {
+                "dark-main-color": "#161A1E",
+                "light-main-color": "#1E2026",
+                "selected-item-color": "#F0B90B",
+                "app-yellow": "#FCD535",
+                "app-red": "#F6465D",
+                "app-green": "#0ECB81",
+                "app-purple": "#EA3DF7",
+                "app-white": "#EAECEF",
+                "app-regular-texts": "#848E9C",
+                "app-regular-numbers": "#B7BDC6",
+                "app-bg": "#F5F5F5",
+            },
+        },
+    },
+    plugins: [
+        // ...
+        flowbite.plugin(),
+    ],
+};
