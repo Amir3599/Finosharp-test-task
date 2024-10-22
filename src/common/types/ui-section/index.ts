7a2b5b2113b7e9351b10961b0c05e26be462fe94
Amir3599 <amir.a.koochaki@gmail.com>
2024-10-23 01:59:05 +0330

    Update index.ts

--- src/common/types/ui-section/index.ts
+++ src/common/types/ui-section/index.ts
@@ -2,3 +2,8 @@ export interface UIsectionType {
     loading?: boolean
     theme?: "dark" | "light"
 }
+
+export interface IconProps {
+    svgProps?: React.SVGProps<SVGSVGElement>
+    color?: string
+}
