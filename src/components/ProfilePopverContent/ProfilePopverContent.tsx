bfb9a604e91c8d53b7d886c90649954568f40378
Amir3599 <amir.a.koochaki@gmail.com>
2024-10-23 01:59:22 +0330

    Create ProfilePopverContent.tsx

---
+++ src/components/ProfilePopverContent/ProfilePopverContent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProfilePopverContent = () => {
+    return (
+        <div className='p-4'>ProfilePopverContent</div>
+    )
+}
+
+export default ProfilePopverContent
